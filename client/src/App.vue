31cbb6bef436b41d3d981c48972b2df719148e27
최현석 <43382559+hschoi1104@users.noreply.github.com>
2019-11-19 01:46:11 +0900

    RESTAPI+CURD

---
+++ client/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app id="inspire">
+
+    <v-app-bar app color="indigo" dark>
+      <v-app-bar-nav-icon/>
+      <v-toolbar-title>Board</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+
+  </v-app>
+  
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ client/src/views/Write.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-container>
+    <v-form>
+      <v-row>
+        <v-col>
+          <v-text-field v-model="title" placeholder="제목" solo></v-text-field>
+          <v-text-field v-model="author" placeholder="작성자" solo></v-text-field>
+          <v-textarea v-model="content" placeholder="내용" solo></v-textarea>
+        </v-col>
+      </v-row>
+      <v-row>
+          <v-col class="d-flex flex-row-reverse">
+              <v-btn @click="onWriteClick">Write</v-btn>
+          </v-col>
+      </v-row>
+    </v-form>
+  </v-container>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            title: "",
+            content: "",
+            author: "",
+        }
+    },
+    methods: {
+        onWriteClick() {
+            axios.post('http://localhost:8000/api/post', {
+                title: this.title,
+                contents: this.content,
+                author: this.author,
+            })
+            .then((res) => {
+                this.$router.push('/')
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+        }
+    }
+};
+</script>
